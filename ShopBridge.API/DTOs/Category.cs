c958a96166bd4b266686b041beb8d7a1b5042107
meuequalsD <nirmalkrishnav@gmail.com>
2021-07-04 20:18:08 +0530

    business validations

--- ShopBridge.API/DTOs/Category.cs
+++ ShopBridge.API/DTOs/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ShopBridge.DTOs
@@ -7,7 +8,9 @@ namespace ShopBridge.DTOs
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Category is required")]
         public string Code { get; set; }
+        [Required(ErrorMessage = "Category Name is required")]
         public string CategoryName { get; set; }
 
     }
--- ShopBridge.API/DTOs/Inventory.cs
+++ ShopBridge.API/DTOs/Inventory.cs
@@ -6,17 +6,25 @@ namespace ShopBridge.DTOs
     public class Inventory
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Name is required")] 
+
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(5)]
         public string Name { get; set; }
         public string Description { get; set; }
         [Required(ErrorMessage = "Category is required")]
         public Category Category { get; set; }
         [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, 99999, ErrorMessage = "The Quantity {0} must be greater than {1}.")]
         public int Quantity { get; set; }
+        
         [Required(ErrorMessage = "Unit is required")]
+        [MinLength(1)]
         public string Unit { get; set; }
+
         [Required(ErrorMessage = "Price is required")]
+        [Range(1, 99999, ErrorMessage = "The field {0} must be greater than {1}.")]
         public double Price { get; set; }
+
         [Required(ErrorMessage = "Status is required")]
         public int Status { get; set; }
         public bool IsActive { get; set; } = true;
